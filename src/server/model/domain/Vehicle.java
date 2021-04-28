a4d3f5228c5070415c59174d7e8a4ddcbe5f549b
juskis4 <ljuskevicius2002@gmail.com>
2021-04-28 11:44:02 +0200

    Vehicle Class

---
+++ src/server/model/domain/Vehicle.java
@@ -0,0 +1,36 @@
+package server.model.domain;
+
+public class Vehicle
+{
+  private String LicenseNo;
+  private String Color;
+  private String CarBrand;
+
+  public Vehicle(String licenseNo, String color, String carBrand)
+  {
+    this.LicenseNo = licenseNo;
+    this.Color = color;
+    this.CarBrand = carBrand;
+  }
+
+  public String getCarBrand()
+  {
+    return CarBrand;
+  }
+
+  public String getColor()
+  {
+    return Color;
+  }
+
+  public String getLicenseNo()
+  {
+    return LicenseNo;
+  }
+
+  @Override public String toString()
+  {
+    return "Vehicle{" + "LicenseNo='" + LicenseNo + '\'' + ", Color='" + Color
+        + '\'' + ", CarBrand='" + CarBrand + '\'' + '}';
+  }
+}
