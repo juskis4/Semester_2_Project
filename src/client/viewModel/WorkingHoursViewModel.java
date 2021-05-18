fe4a12fb4451c0d7c6bbf50fffea996251dd3ab3
juskis4 <ljuskevicius2002@gmail.com>
2021-05-18 13:20:56 +0200

    WorkingHoursView and viewModel

--- src/client/viewModel/WorkingHoursViewModel.java
+++ src/client/viewModel/WorkingHoursViewModel.java
@@ -1,14 +1,32 @@
 package client.viewModel;
 
 import client.model.Model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-public class WorkingHoursViewModel {
+public class WorkingHoursViewModel
+{
+
+    private StringProperty errorProperty;
     private Model model;
 
     public WorkingHoursViewModel(Model model)
     {
         this.model = model;
+        errorProperty = new SimpleStringProperty("");
+    }
+    public void reset()
+    {
+        errorProperty.set("");
     }
 
+    public Model getModel() {
+        return model;
+    }
+
+    public StringProperty getErrorProperty()
+    {
+        return errorProperty;
+    }
 
 }
