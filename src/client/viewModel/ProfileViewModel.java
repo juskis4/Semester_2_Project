740752788536a5b6250b6f71c2cfc697b65caba7
BaicoianuIoanSorin <71268639+BaicoianuIoanSorin@users.noreply.github.com>
2021-05-18 12:21:35 +0200

    ProfileView changes

--- src/client/viewModel/ProfileViewModel.java
+++ src/client/viewModel/ProfileViewModel.java
@@ -1,16 +1,75 @@
 package client.viewModel;
 
 import client.model.Model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.rmi.RemoteException;
 
 public class ProfileViewModel {
 
-    public ProfileViewModel(Model model)
+    private StringProperty userNameProperty;
+    private StringProperty licenseNoProperty;
+    private StringProperty actualFirstNameProperty;
+    private StringProperty actualSecondNameProperty;
+    private StringProperty firstNameProperty;
+    private StringProperty secondNameProperty;
+    private Model model;
+
+    public ProfileViewModel(Model model) throws RemoteException
     {
 
+        this.model = model;
+        userNameProperty = new SimpleStringProperty(model.getUserByUserName().getUsername());
+        licenseNoProperty = new SimpleStringProperty(model.getUserByUserName().getVehicle().getLicenseNo());
+        actualFirstNameProperty = new SimpleStringProperty(model.getUserByUserName().getFirstname());
+        actualSecondNameProperty = new SimpleStringProperty(model.getUserByUserName().getLastname());
+        firstNameProperty = new SimpleStringProperty("");
+        secondNameProperty = new SimpleStringProperty("");
     }
 
-    public void reset()
+    public void reset() throws RemoteException
     {
+        userNameProperty = new SimpleStringProperty(model.getUserByUserName().getUsername());
+        licenseNoProperty = new SimpleStringProperty(model.getUserByUserName().getVehicle().getLicenseNo());
+        actualFirstNameProperty = new SimpleStringProperty(model.getUserByUserName().getFirstname());
+        actualSecondNameProperty = new SimpleStringProperty(model.getUserByUserName().getLastname());
+        firstNameProperty = new SimpleStringProperty("");
+        secondNameProperty = new SimpleStringProperty("");
+    }
 
+    public StringProperty getUserNameProperty()
+    {
+        return userNameProperty;
+    }
+
+    public StringProperty getLicenseNoProperty()
+    {
+        return licenseNoProperty;
+    }
+
+    public StringProperty getActualFirstNameProperty()
+    {
+        return actualFirstNameProperty;
+    }
+
+    public StringProperty getActualSecondNameProperty()
+    {
+        return actualSecondNameProperty;
+    }
+
+    public StringProperty getFirstNameProperty()
+    {
+        return firstNameProperty;
+    }
+
+    public StringProperty getSecondNameProperty()
+    {
+        return secondNameProperty;
+    }
+
+    public void changeNames()
+    {
+        model.registerFirstAndLastName(firstNameProperty.get(),secondNameProperty.get(),userNameProperty.get());
     }
 }
