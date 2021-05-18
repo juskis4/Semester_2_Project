ab65b0ce87d2b968c6291aad69ea59e6a3c183d4
rycka13 <71268611+rycka13@users.noreply.github.com>
2021-05-18 10:59:13 +0200

    ParkingLot

--- src/client/view/ParkingLotViewController.java
+++ src/client/view/ParkingLotViewController.java
@@ -4,6 +4,9 @@ import client.viewModel.ParkingLotViewModel;
 import client.viewModel.RegisterViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
+import server.model.domain.ParkingLot;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 import java.awt.*;
 
@@ -36,4 +39,43 @@ public class ParkingLotViewController {
     private ViewHandler viewHandler;
     private Region root;
     private ParkingLotViewModel viewModel;
+
+    public ParkingLotViewController(){
+
+    }
+
+    public void init(ViewHandler viewHandler, ParkingLotViewModel viewModel, Region root)
+    {
+        this.viewModel = viewModel;
+        this.viewHandler = viewHandler;
+        this.root = root;
+
+        spaceA1.setTextFill(Color.rgb(0,255,0));
+        spaceA2.setTextFill(Color.rgb(0,255,0));
+        spaceA3.setTextFill(Color.rgb(0,255,0));
+        spaceA4.setTextFill(Color.rgb(0,255,0));
+        spaceA5.setTextFill(Color.rgb(0,255,0));
+        spaceA6.setTextFill(Color.rgb(0,255,0));
+
+        usernameField.textProperty().bindBidirectional(viewModel.getUserNameProperty());
+        passwordField.textProperty().bindBidirectional(viewModel.getPasswordProperty());
+        errorLabel.textProperty().bindBidirectional(viewModel.getErrorProperty());
+    }
+
+    public Region getRoot()
+    {
+        return root;
+    }
+
+    public void reset()
+    {
+        viewModel.reset();
+    }
+
+    @FXML private void onLogin()
+    {
+        if(viewModel.login()){
+
+        }
+    }
 }
